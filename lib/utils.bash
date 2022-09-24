6ca006f04b062dc9363872cb4f623c3fc81cda8c
Andreas Weber <weber.andreas@gmail.com>
2022-09-24 14:12:30 +0200

    fix: type in url

--- lib/utils.bash
+++ lib/utils.bash
@@ -66,7 +66,7 @@ download_release() {
 
   arch="$(get_arch)"
   platform="$(get_platform)"
-  url="$GH_REPO}/releases/download/v${version}/mani_${version}_${platform}_${arch}.tar.gz"
+  url="${GH_REPO}/releases/download/v${version}/mani_${version}_${platform}_${arch}.tar.gz"
 
   echo "* Downloading $TOOL_NAME release $version..."
   curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
